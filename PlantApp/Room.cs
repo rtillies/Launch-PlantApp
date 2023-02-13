ee7ac80c0f7e7f8f961ed8dcfc04e8d520e8a814
Richard T <richard@turing.edu>
2023-02-13 13:58:17 -0500

    Add project files.

---
+++ PlantApp/Room.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantApp
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string HasSunlight { get; set; }
+        public List<Plant> Plants { get; set; }
+    }
+}
